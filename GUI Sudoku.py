fde4c4a1acebcf1591bf80388658efc00e68df4e
DGarciaMedina <43907632+DGarciaMedina@users.noreply.github.com>
2018-10-06 14:41:51 +0100

    Added files required for GUI to work with example and .exe file

---
+++ GUI Sudoku.py
@@ -0,0 +1,141 @@
+from tkinter import *
+import tkinter.messagebox
+from Sudoku2 import solver
+
+root = Tk()
+root.geometry("1000x850+300+0")
+root.title("Sudoku Solver")
+root.tk.call('tk', 'scaling', 2.0)
+
+
+# =====================Functions==========================
+
+def Solve():
+    for i in range(9):
+        for j in range(9):
+            eval("btn" + str(i) + str(j) + ".configure(fg='black')")
+
+    square = [0] * 9
+
+    for i in range(9):
+        for j in range(9):
+
+            if eval("btn" + str(i) + str(j) + ".get()") == "" or eval("btn" + str(i) + str(j) + ".get()").isdigit():
+                if len(eval("btn" + str(i) + str(j) + ".get()")) == 0:
+                    eval("btn" + str(i) + str(j) + ".delete(0,'end')")
+                    eval("btn" + str(i) + str(j) + ".insert(0,'0')")
+            else:
+                reset()
+                tkinter.messagebox.showerror("Submission Error",
+                                             "Please, only input integer numbers. The characters A-Z,.-?!/#@ are not allowed.")
+                return
+
+    square[0] = list(
+        [[int(btn00.get())], [int(btn10.get())], [int(btn20.get())], [int(btn30.get())], [int(btn40.get())],
+         [int(btn50.get())], [int(btn60.get())], [int(btn70.get())], [int(btn80.get())]])
+    square[1] = list(
+        [[int(btn01.get())], [int(btn11.get())], [int(btn21.get())], [int(btn31.get())], [int(btn41.get())],
+         [int(btn51.get())], [int(btn61.get())], [int(btn71.get())], [int(btn81.get())]])
+    square[2] = list(
+        [[int(btn02.get())], [int(btn12.get())], [int(btn22.get())], [int(btn32.get())], [int(btn42.get())],
+         [int(btn52.get())], [int(btn62.get())], [int(btn72.get())], [int(btn82.get())]])
+    square[3] = list(
+        [[int(btn03.get())], [int(btn13.get())], [int(btn23.get())], [int(btn33.get())], [int(btn43.get())],
+         [int(btn53.get())], [int(btn63.get())], [int(btn73.get())], [int(btn83.get())]])
+    square[4] = list(
+        [[int(btn04.get())], [int(btn14.get())], [int(btn24.get())], [int(btn34.get())], [int(btn44.get())],
+         [int(btn54.get())], [int(btn64.get())], [int(btn74.get())], [int(btn84.get())]])
+    square[5] = list(
+        [[int(btn05.get())], [int(btn15.get())], [int(btn25.get())], [int(btn35.get())], [int(btn45.get())],
+         [int(btn55.get())], [int(btn65.get())], [int(btn75.get())], [int(btn85.get())]])
+    square[6] = list(
+        [[int(btn06.get())], [int(btn16.get())], [int(btn26.get())], [int(btn36.get())], [int(btn46.get())],
+         [int(btn56.get())], [int(btn66.get())], [int(btn76.get())], [int(btn86.get())]])
+    square[7] = list(
+        [[int(btn07.get())], [int(btn17.get())], [int(btn27.get())], [int(btn37.get())], [int(btn47.get())],
+         [int(btn57.get())], [int(btn67.get())], [int(btn77.get())], [int(btn87.get())]])
+    square[8] = list(
+        [[int(btn08.get())], [int(btn18.get())], [int(btn28.get())], [int(btn38.get())], [int(btn48.get())],
+         [int(btn58.get())], [int(btn68.get())], [int(btn78.get())], [int(btn88.get())]])
+
+    A, B = solver(square)
+
+    Found = False
+
+    for i in range(9):
+        for j in range(9):
+            if eval("btn" + str(i) + str(j) + ".get()") == str(0):
+                eval("btn" + str(i) + str(j) + ".configure(fg='red')")
+                eval("btn" + str(i) + str(j) + ".delete(0,'end')")
+                eval("btn" + str(i) + str(j) + ".insert(0,'" + str(A[j][i]) + "')")
+
+    for i in range(9):
+        for j in range(9):
+            if len(eval("btn" + str(i) + str(j) + ".get()")) > 1:
+                delete_arrays()
+
+                tkinter.messagebox.showerror("Submission Error",
+                                             "The Sudoku could not be solved because of the following reasons:\n\n"
+                                             "     1) The grid is empty\n     2) The sudoku is too complex\n\n"
+                                             "Please, try by inserting new values.")
+
+                return
+
+
+def reset():
+    for i in range(9):
+        for j in range(9):
+            eval("btn" + str(i) + str(j) + ".delete(0,'end')")
+            eval("btn" + str(i) + str(j) + ".configure(fg='black')")
+
+
+def delete_arrays():
+    for i in range(9):
+        for j in range(9):
+            if len(eval("btn" + str(i) + str(j) + ".get()")) > 1:
+                eval("btn" + str(i) + str(j) + ".delete(0,'end')")
+
+
+# ====================Widgets==============================
+
+Top = Frame(root, width=800, height=75, relief=SUNKEN, bg="white")
+Top.pack(side=TOP, fill=X)
+
+BottomFrame = Frame(root, width=800, height=425, relief=SUNKEN, bg="white")
+BottomFrame.pack(side=TOP, fill=X, ipady=2)
+
+BottomSudoku = Frame(BottomFrame, width=400, height=200, relief=SUNKEN, bg="white", bd=10)
+BottomSudoku.pack(side=TOP, fill=Y, padx=2, pady=2)
+
+BottomInfo = Frame(BottomFrame, width=400, height=200, relief=FLAT, bg="white", bd=2)
+BottomInfo.pack(side=TOP, fill=Y, padx=2, pady=2)
+
+lblInfo = Label(Top, font=("Kozuka Gothic Pro EL", 50, "bold"), text="Sudoku Solver", fg="dark blue", bg="white", bd=10,
+                anchor="w")
+lblInfo.pack()
+
+Name = Label(Top, font=("Kozuka Gothic Pro EL", 8, "bold"), text="Diego García Medina © 2017", fg="black", bg="white",
+             bd=10, anchor="w")
+Name.pack()
+
+Instructions = Label(BottomInfo, font=("Kozuka Gothic Pro EL", 12),
+                     text="\nPlease input the given numbers\n in the grid above and press the\n 'Solve' button to complete it.\n",
+                     bg="white", fg="black")
+Instructions.pack(side=TOP)
+
+SubmitButton = Button(BottomFrame, text="Solve", justify="center", font=("Kozuka Gothic Pro EL", 20, "bold"),
+                      command=Solve, padx=100, cursor="arrow", highlightthickness=5)
+SubmitButton.pack(side=RIGHT)
+
+ResetButton = Button(BottomFrame, text="Reset", fg="gray40", justify="center",
+                     font=("Kozuka Gothic Pro EL", 20, "bold"),
+                     command=reset, padx=100, cursor="arrow")
+ResetButton.pack(side=LEFT)
+
+for j in range(9):
+    for i in range(9):
+        exec("btn" + str(i) + str(j) + "=Entry(BottomSudoku, font=('calibri', 12, 'bold'), justify='center', width=3)")
+        eval("btn" + str(i) + str(j) + ".grid(row=" + str(i) + ", column=" + str(j) + ")")
+
+
+root.mainloop()
---
+++ Sudoku2.py
@@ -0,0 +1,350 @@
+import numpy as np
+
+square = [0] * 9
+
+square[0] = list([[0], [7], [0], [2], [5], [0], [4], [0], [0]])
+square[1] = list([[8], [0], [0], [0], [0], [0], [9], [0], [3]])
+square[2] = list([[0], [0], [0], [0], [0], [3], [0], [7], [0]])
+square[3] = list([[7], [0], [0], [0], [0], [4], [0], [2], [0]])
+square[4] = list([[1], [0], [0], [0], [0], [0], [0], [0], [7]])
+square[5] = list([[0], [4], [0], [5], [0], [0], [0], [0], [8]])
+square[6] = list([[0], [9], [0], [6], [0], [0], [0], [0], [0]])
+square[7] = list([[4], [0], [1], [0], [0], [0], [0], [0], [5]])
+square[8] = list([[0], [0], [7], [0], [8], [2], [0], [3], [0]])
+
+
+def check_row(square, row):
+    NumList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    numbers = square[row][:]
+    numbers_used = []
+
+    for i in range(9):
+        if (NumList[i] in numbers) and (i < 8) and (NumList[i] not in numbers_used):
+            numbers_used.append(NumList[i])
+            pass
+        elif (NumList[i] in numbers) and (i == 8) and (NumList[i] not in numbers_used):
+            return True
+        else:
+            return False
+
+
+def check_column(square, column):
+    NumList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    numbers = square[:][column]
+    numbers_used = []
+
+    for i in range(9):
+        if (NumList[i] in numbers) and (i < 8) and (NumList[i] not in numbers_used):
+            numbers_used.append(NumList[i])
+            pass
+        elif (NumList[i] in numbers) and (i == 8) and (NumList[i] not in numbers_used):
+            return True
+        else:
+            return False
+
+
+def check_all_rows(square):
+    RowsOk = True
+
+    for i in range(9):
+        A = check_row(square, i)
+        if A == False:
+            RowsOk = False
+
+    return RowsOk
+
+
+def check_all_columns(square):
+    ColumnsOk = True
+
+    for i in range(9):
+        A = check_column(square, i)
+        if A == False:
+            ColumnsOk = False
+
+    return ColumnsOk
+
+
+def set_possible_outcomes(square):
+    for i in range(9):
+        for j in range(9):
+            if square[i][j] == [0]:
+                square[i][j] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    return square
+
+
+def solver(square):
+    square2 = set_possible_outcomes(square)
+
+    Solved = False
+    counter = 0
+
+    while counter < 50 and (Solved == False):
+        counter += 1
+
+        for repetition1 in range(50):
+            for i in range(3):
+                for j in range(3):
+                    NumbersSubsquare = []
+                    for k in range(i * 3, (i * 3) + 3):
+                        for m in range(j * 3, (j * 3) + 3):
+                            if type(square2[k][m]) == int:
+                                NumbersSubsquare.append(square2[k][m])
+
+                    for k in range(i * 3, i * 3 + 3):
+                        for m in range(j * 3, j * 3 + 3):
+                            if type(square2[k][m]) == list:
+                                for possibility in square2[k][m]:
+                                    if possibility in NumbersSubsquare:
+                                        square2[k][m].remove(possibility)
+                            if type(square2[k][m]) == list:
+                                if len(square2[k][m]) == 1:
+                                    for a in square2[k][m]:
+                                        square2[k][m] = a
+
+            for i in range(9):
+                NumbersRow = []
+
+                for j in range(9):
+
+                    if type(square2[i][j]) == int:
+                        NumbersRow.append(square2[i][j])
+
+                for j in range(9):
+                    if type(square2[i][j]) == list:
+                        if len(square2[i][j]) > 1:
+                            for possibility in square2[i][j]:
+                                if possibility in NumbersRow:
+                                    square2[i][j].remove(possibility)
+
+                    if type(square2[i][j]) == list:
+                        if len(square2[i][j]) == 1:
+                            for a in square2[i][j]:
+                                square2[i][j] = a
+
+            for j in range(9):
+                NumbersColumn = []
+
+                for i in range(9):
+
+                    if type(square2[i][j]) == int:
+                        NumbersColumn.append(square2[i][j])
+
+                for i in range(9):
+                    if type(square2[i][j]) == list:
+                        if len(square2[i][j]) > 1:
+                            for possibility in square2[i][j]:
+                                if possibility in NumbersColumn:
+                                    square2[i][j].remove(possibility)
+                    if type(square2[i][j]) == list:
+                        if len(square2[i][j]) == 1:
+                            for a in square2[i][j]:
+                                square2[i][j] = a
+
+        # for p in range(10):
+        #     for i in range(9):
+        #         for j in range(9):
+        #
+        #             if counter < 5:
+        #                 for k in square2:
+        #                     print(k)
+        #
+        #             print("\n", square2[i], "\n", i, j, counter)
+        #
+        #             if type(square2[i][j]) == list:
+        #                 for r in range(len(square2[i][j])):
+        #
+        #                     print(r)
+        #                     # print(square2[i],"*\n",r)
+        #                     # print(square2[i][j],"**\n",r,type(square2[i][j]))
+        #                     if type(square2[i][j]) == list:
+        #                         square2 = check_squares2(square2, i, j, r)
+        #                     print(square2, "*\n", r)
+        #                     # print(square2[i][j], "**\n", r, type(square2[i][j]))
+
+
+        if (check_all_columns(square2) == True) and (check_all_rows(square2) == True):
+            Solved = True
+            break
+
+    return square2, counter
+
+
+def check_squares2(square2, i, j, r):
+    # print(square2)
+
+    if type(square2[i][j]) == list:
+
+        if i < 3 and j < 3:
+
+            for k in range(0, 3):
+                for m in range(0, 3):
+
+                    if type(square2[k][m]) == list:
+                        if i == k and j == m:
+                            pass
+                        else:
+                            for possibility in square2[k][m]:
+                                # print(square2[i][j][r], possibility, square2[i][j][r] == possibility)
+                                if square2[i][j][r] == possibility:
+                                    return square2
+
+            square2[i][j] = square2[i][j][r]
+            return square2
+
+        elif i >= 3 and i < 6 and j < 3:
+
+            for k in range(3, 6):
+                for m in range(0, 3):
+
+                    if type(square2[k][m]) == list:
+                        if i == k and j == m:
+                            pass
+                        else:
+                            for possibility in square2[k][m]:
+                                # print(square2[i][j][r], possibility, square2[i][j][r] == possibility)
+                                if square2[i][j][r] == possibility:
+                                    return square2
+
+            square2[i][j] = square2[i][j][r]
+            return square2
+
+        elif i >= 6 and j < 3:
+
+            for k in range(6, 9):
+                for m in range(0, 3):
+
+                    if type(square2[k][m]) == list:
+                        if i == k and j == m:
+                            pass
+                        else:
+                            for possibility in square2[k][m]:
+                                # print(square2[i][j][r], possibility, square2[i][j][r] == possibility)
+                                if square2[i][j][r] == possibility:
+                                    return square2
+
+            square2[i][j] = square2[i][j][r]
+            return square2
+
+        elif i < 3 and j < 6 and j >= 3:
+
+            for k in range(0, 3):
+                for m in range(3, 6):
+
+                    if type(square2[k][m]) == list:
+                        if i == k and j == m:
+                            pass
+                        else:
+                            for possibility in square2[k][m]:
+                                # print(square2[i][j][r], possibility, square2[i][j][r] == possibility)
+                                if square2[i][j][r] == possibility:
+                                    return square2
+
+            square2[i][j] = square2[i][j][r]
+            return square2
+
+
+        elif i >= 3 and i < 6 and j >= 3 and j < 6:
+
+            for k in range(3, 6):
+                for m in range(3, 6):
+
+                    if type(square2[k][m]) == list:
+                        if i == k and j == m:
+                            pass
+                        else:
+                            for possibility in square2[k][m]:
+                                # print(square2[i][j][r], possibility, square2[i][j][r] == possibility)
+                                if square2[i][j][r] == possibility:
+                                    return square2
+
+            square2[i][j] = square2[i][j][r]
+            return square2
+
+        elif i > 6 and j >= 3 and j < 6:
+
+            for k in range(6, 9):
+                for m in range(3, 6):
+
+                    if type(square2[k][m]) == list:
+                        if i == k and j == m:
+                            pass
+                        else:
+                            for possibility in square2[k][m]:
+                                # print(square2[i][j][r], possibility, square2[i][j][r] == possibility)
+                                if square2[i][j][r] == possibility:
+                                    return square2
+
+            square2[i][j] = square2[i][j][r]
+            return square2
+
+        elif i < 3 and j >= 6:
+
+            for k in range(0, 3):
+                for m in range(6, 9):
+
+                    if type(square2[k][m]) == list:
+                        if i == k and j == m:
+                            pass
+                        else:
+                            for possibility in square2[k][m]:
+                                # print(square2[i][j][r], possibility, square2[i][j][r] == possibility)
+                                if square2[i][j][r] == possibility:
+                                    return square2
+
+            square2[i][j] = square2[i][j][r]
+            return square2
+
+        elif i >= 3 and i < 6 and j >= 6:
+
+            for k in range(3, 6):
+                for m in range(6, 9):
+
+                    if type(square2[k][m]) == list:
+                        if i == k and j == m:
+                            pass
+                        else:
+                            for possibility in square2[k][m]:
+                                # print(square2[i][j][r], possibility, square2[i][j][r] == possibility)
+                                if square2[i][j][r] == possibility:
+                                    return square2
+            # print("\n hey")
+            square2[i][j] = square2[i][j][r]
+            return square2
+
+        elif i >= 6 and j >= 6:
+
+            for k in range(6, 9):
+                for m in range(6, 9):
+
+                    if type(square2[k][m]) == list:
+                        if i == k and j == m:
+                            pass
+                        else:
+                            for possibility in square2[k][m]:
+                                # print(square2[i][j][r], possibility, square2[i][j][r] == possibility)
+                                if square2[i][j][r] == possibility:
+                                    return square2
+
+            square2[i][j] = square2[i][j][r]
+            return square2
+    else:
+        return square2
+
+
+def columns2(square2, i, r):
+    pass
+
+
+A, B = solver(square)
+
+for i in A:
+    print(i)
+
+print("There were", B, "iterations required.")
+
+# A=[1,2,3,4,5]
+# print(A[0])
